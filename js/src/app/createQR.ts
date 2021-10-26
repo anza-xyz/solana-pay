7dae0359d22a947f3addfb1cacaf6ba6270e5e37
Jordan Sexton <jordan@jordansexton.com>
2021-10-26 16:29:55 -0500

    add qr codes

--- js/src/app/createQR.ts
+++ js/src/app/createQR.ts
@@ -1,1 +1,6 @@
-export {};
+import QRCode from 'qrcode-svg';
+
+export function createQR(content: string | QRCode.Options): string {
+    if (typeof content === 'string') return new QRCode(content).svg();
+    return new QRCode({ pretty: false, ...content}).svg();
+}
