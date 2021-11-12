35128e725ea636b6f1404e1970f6cd3c4af16c6f
Jordan Sexton <jordan@jordansexton.com>
2021-11-12 20:16:45 +0000

    add encodeURL function

---
+++ js/src/app/encodeURL.ts
@@ -0,0 +1,38 @@
+import { PublicKey } from '@solana/web3.js';
+import BigNumber from 'bignumber.js';
+
+export function encodeURL(
+    recipient: PublicKey,
+    amount: BigNumber,
+    token?: PublicKey,
+    references?: PublicKey[],
+    label?: string,
+    message?: string,
+    memo?: string,
+): string {
+    let url = `solana:${String(recipient)}?amount=${String(amount)}`;
+
+    if (token) {
+        url += `?spl-token=${String(token)}`;
+    }
+
+    if (references?.length) {
+        for (const reference of references) {
+            url += `?reference=${String(reference)}`;
+        }
+    }
+
+    if (label) {
+        url += `?label=${String(label)}`;
+    }
+
+    if (message) {
+        url += `?label=${String(message)}`;
+    }
+
+    if (memo) {
+        url += `?label=${String(memo)}`;
+    }
+
+    return url;
+}
--- js/src/app/index.ts
+++ js/src/app/index.ts
@@ -1,1 +1,2 @@
 export * from './createQR';
+export * from './encodeURL';
