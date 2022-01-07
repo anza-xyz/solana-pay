f2b7b46dc0a8a481233069fa9e745affde9450cf
Jordan Sexton <jordan@jordansexton.com>
2022-01-06 22:55:46 -0600

    setup wallet adapter

--- point-of-sale/src/App.tsx
+++ point-of-sale/src/App.tsx
@@ -1,13 +1,17 @@
-import { Keypair } from '@solana/web3.js';
-import React, { FC, ReactNode, useEffect } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter, TorusWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { PublicKey } from '@solana/web3.js';
+import React, { FC, ReactNode, useEffect, useMemo } from 'react';
 import { ConfigProvider } from './hooks/useConfig';
-import { ConnectionProvider } from './hooks/useConnection';
 import { PaymentProvider, PaymentStatus, usePayment } from './hooks/usePayment';
 import { ThemeProvider } from './hooks/useTheme';
 import { AmountPage } from './pages/AmountPage';
 import { ConfirmationPage } from './pages/ConfirmationPage';
 import { QRPage } from './pages/QRPage';
 import { toggleFullscreen } from './utils/toggleFullscreen';
+
+require('@solana/wallet-adapter-react-ui/styles.css');
 
 export const App: FC = () => {
     useEffect(() => {
@@ -28,19 +32,27 @@ export const App: FC = () => {
 };
 
 const Context: FC<{ children: ReactNode }> = ({ children }) => {
+    const wallets = useMemo(() => [new PhantomWalletAdapter(), new TorusWalletAdapter()], []);
+    const account = useMemo(() => new PublicKey('GvHeR432g7MjN9uKyX3Dzg66TqwrEWgANLnnFZXMeyyj'), []);
+    const token = useMemo(() => new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr'), []);
+
     return (
         <ThemeProvider>
-            <ConnectionProvider>
-                <ConfigProvider
-                    account={Keypair.generate().publicKey}
-                    token={Keypair.generate().publicKey}
-                    symbol="USDC"
-                    decimals={6}
-                    minDecimals={2}
-                    label="Starbucks"
-                >
-                    <PaymentProvider>{children}</PaymentProvider>
-                </ConfigProvider>
+            <ConnectionProvider endpoint="https://psytrbhymqlkfrhudd.dev.genesysgo.net:8899/">
+                <WalletProvider wallets={wallets} autoConnect>
+                    <WalletModalProvider>
+                        <ConfigProvider
+                            account={account}
+                            token={token}
+                            symbol="USDC"
+                            decimals={6}
+                            minDecimals={2}
+                            label="Starbucks"
+                        >
+                            <PaymentProvider>{children}</PaymentProvider>
+                        </ConfigProvider>
+                    </WalletModalProvider>
+                </WalletProvider>
             </ConnectionProvider>
         </ThemeProvider>
     );
