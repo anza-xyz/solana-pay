996098abbce376b0d4cc65d0032478dd772dc72d
Colin Ogoo <ogoo.colin@gmail.com>
2022-01-07 15:12:39 +0000

    test(createtransaction): error checks for transferring native SOL

--- core/test/createTransaction.test.ts
+++ core/test/createTransaction.test.ts
@@ -1,11 +1,11 @@
-import { clusterApiUrl, Connection, Keypair, PublicKey } from '@solana/web3.js';
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram } from '@solana/web3.js';
 import BigNumber from 'bignumber.js';
-import { createTransaction, CreateTransactionError } from '../../src/wallet/createTransaction';
+import { createTransaction } from '../../src/wallet/createTransaction';
 
 describe('createTransaction', () => {
     let connection: Connection;
 
-    beforeEach(() => {
+    beforeAll(() => {
         const endpoint = clusterApiUrl('devnet');
         connection = new Connection(endpoint, 'confirmed');
     });
@@ -37,6 +37,81 @@ describe('createTransaction', () => {
                         {}
                     )
             ).rejects.toThrow('recipient not found');
+        });
+    });
+
+    describe('transfer', () => {
+        let wallet: Keypair;
+
+        beforeAll(async () => {
+            wallet = Keypair.generate();
+
+            const airdropSignature = await connection.requestAirdrop(wallet.publicKey, LAMPORTS_PER_SOL / 1000);
+            await connection.confirmTransaction(airdropSignature);
+        });
+
+        describe('when transferring native SOL', () => {
+            describe('errors', () => {
+                it('throws an error on invalid payer owner', async () => {
+                    expect.assertions(1);
+
+                    await expect(async () => {
+                        return await createTransaction(
+                            connection,
+                            new PublicKey(SystemProgram.programId),
+                            new PublicKey('mvines9iiHiQTysrwkJjGf2gb9Ex9jXJX8ns3qwf2kN'),
+                            new BigNumber(1),
+                            {}
+                        );
+                    }).rejects.toThrow('payer owner invalid');
+                });
+
+                it.todo('throws an error on executable payer');
+
+                it('throws an error on invalid recipient owner', async () => {
+                    expect.assertions(1);
+
+                    await expect(async () => {
+                        return await createTransaction(
+                            connection,
+                            new PublicKey('mvines9iiHiQTysrwkJjGf2gb9Ex9jXJX8ns3qwf2kN'),
+                            new PublicKey(SystemProgram.programId),
+                            new BigNumber(1),
+                            {}
+                        );
+                    }).rejects.toThrow('recipient owner invalid');
+                });
+
+                it.todo('throws an error on executable recipient');
+
+                it('throws an error on invalid decimal amount', async () => {
+                    expect.assertions(1);
+
+                    await expect(async () => {
+                        return await createTransaction(
+                            connection,
+                            wallet.publicKey,
+                            new PublicKey('mvines9iiHiQTysrwkJjGf2gb9Ex9jXJX8ns3qwf2kN'),
+                            new BigNumber(1.0000000000001),
+                            {}
+                        );
+                    }).rejects.toThrow('amount decimals invalid');
+                });
+
+                it('throws an error on insufficient funds', async () => {
+                    expect.assertions(1);
+
+                    await expect(async () => {
+                        return await createTransaction(
+                            connection,
+                            wallet.publicKey,
+                            new PublicKey('mvines9iiHiQTysrwkJjGf2gb9Ex9jXJX8ns3qwf2kN'),
+                            new BigNumber(100),
+                            {}
+                        );
+                    }).rejects.toThrow('insufficient funds');
+                });
+            });
         });
     });
 });
