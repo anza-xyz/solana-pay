9fb41585249344fc1ba8c313070ce130ff2e8125
Jordan Sexton <jordan@jordansexton.com>
2022-01-06 21:30:03 -0600

    confirmation page

--- point-of-sale/src/pages/ConfirmationPage.tsx
+++ point-of-sale/src/pages/ConfirmationPage.tsx
@@ -1,13 +1,10 @@
 import React, { FC } from 'react';
 import { PoweredBy } from '../components/PoweredBy';
-import { QRCode } from '../components/QRCode';
-import { useConfig } from '../hooks/useConfig';
 import { usePayment } from '../hooks/usePayment';
-import * as styles from './QRPage.module.css';
+import * as styles from './ConfirmationPage.module.css';
 
 export const ConfirmationPage: FC = () => {
-    const { symbol } = useConfig();
-    const { amount, reset } = usePayment();
+    const { status, reset } = usePayment();
 
     return (
         <div className={styles.root}>
@@ -16,15 +13,7 @@ export const ConfirmationPage: FC = () => {
                     <span className={styles.arrow}>◄</span>Start Over
                 </button>
             </div>
-            <div className={styles.main}>
-                Total
-                <br />
-                {String(amount)}
-                <br />
-                {symbol}
-                <br />
-                <QRCode />
-            </div>
+            <div className={styles.main}>{status}</div>
             <div className={styles.footer}>
                 <PoweredBy />
             </div>
