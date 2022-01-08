065eefa5008592753b3c5c016f54261fd04531b1
Jordan Sexton <jordan@jordansexton.com>
2022-01-08 17:52:27 -0600

    simplify QR options API

--- point-of-sale/src/components/QRCode.tsx
+++ point-of-sale/src/components/QRCode.tsx
@@ -1,4 +1,4 @@
-import { getQROptions } from '@solana/pay';
+import { createQROptions } from '@solana/pay';
 import QRCodeStyling from 'qr-code-styling';
 import React, { FC, useLayoutEffect, useMemo, useRef } from 'react';
 import { useMediaQuery } from 'react-responsive';
@@ -7,23 +7,14 @@ import { useTheme } from '../hooks/useTheme';
 
 export const QRCode: FC = () => {
     const phone = useMediaQuery({ query: '(max-width: 767px)' });
-    const [width, height] = useMemo(
-        () =>
-            phone && typeof window !== 'undefined'
-                ? [window.screen.availWidth - 48, window.screen.availWidth - 48]
-                : [400, 400],
-        [phone]
-    );
+    const size = useMemo(() => (phone && typeof window !== 'undefined' ? window.screen.availWidth - 48 : 400), [phone]);
     const { theme } = useTheme();
     const [background, color] = useMemo(
         () => (theme === 'light' ? ['#EFF2F3', '#2A2A2A'] : ['#2A2A2A', '#EEF5F6']),
         [theme]
     );
     const { url } = usePayment();
-    const options = useMemo(
-        () => (url ? getQROptions(url, width, height, background, color) : {}),
-        [url, width, height, background, color]
-    );
+    const options = useMemo(() => createQROptions(url, size, background, color), [url, size, background, color]);
 
     const qr = useMemo(() => new QRCodeStyling(), []);
     useLayoutEffect(() => qr.update(options), [qr, options]);
