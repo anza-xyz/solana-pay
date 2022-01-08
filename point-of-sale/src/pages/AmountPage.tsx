52c74dfb794e59a1c6b942270e4d4062962ffa08
Jordan Sexton <jordan@jordansexton.com>
2022-01-07 21:22:29 -0600

    responsive amount page

--- point-of-sale/src/pages/AmountPage.tsx
+++ point-of-sale/src/pages/AmountPage.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { Amount } from '../components/Amount';
 import { NumPad } from '../components/NumPad';
 import { PoweredBy } from '../components/PoweredBy';
@@ -10,6 +11,8 @@ export const AmountPage: FC = () => {
     const { symbol } = useConfig();
     const { amount, generate } = usePayment();
 
+    const phone = useMediaQuery({ query: '(max-width: 767px)' });
+
     return (
         <div className={styles.root}>
             <div className={styles.entry}>
@@ -17,16 +20,20 @@ export const AmountPage: FC = () => {
             </div>
             <div className={styles.summary}>
                 <div className={styles.header}>
-                    <div className={styles.title}>Balance Due</div>
-                    <div className={styles.total}>
-                        <div className={styles.totalLeft}>Total</div>
-                        <div className={styles.totalRight}>
-                            <div className={styles.symbol}>{symbol}</div>
-                            <div className={styles.amount}>
-                                <Amount />
+                    {phone ? null : (
+                        <>
+                            <div className={styles.title}>Balance Due</div>
+                            <div className={styles.total}>
+                                <div className={styles.totalLeft}>Total</div>
+                                <div className={styles.totalRight}>
+                                    <div className={styles.symbol}>{symbol}</div>
+                                    <div className={styles.amount}>
+                                        <Amount />
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
+                        </>
+                    )}
                     <button
                         className={styles.button}
                         type="button"
