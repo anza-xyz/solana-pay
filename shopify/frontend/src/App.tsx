3b23b655b73d592839bee885cd556f10af4fcf27
Jordan Sexton <jordan@jordansexton.com>
2022-01-19 23:37:19 -0600

    replace CRA with Parcel

--- shopify/frontend/src/App.tsx
+++ shopify/frontend/src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import './App.css';
 import { Switch, Route } from "react-router-dom";
 import { Landing } from './components/Landing';
 import { PaymentSessionProvider } from './providers/PaymentSessionProvider';
 import { Payment } from './components/Payment';
 
 function App() {
-  return (
-    <PaymentSessionProvider>
-      <Switch>
-        <Route exact path="/">
-          <Landing />
-        </Route>
-        <Route exact path="/error">
-          <h1>Some error!</h1>
-        </Route>
-        <Route exact path="/pay">
-          <Payment />
-        </Route>
-      </Switch>
-    </PaymentSessionProvider>
-  );
+    return (
+        <PaymentSessionProvider>
+            <Switch>
+                <Route exact path="/">
+                    <Landing />
+                </Route>
+                <Route exact path="/error">
+                    <h1>Some error!</h1>
+                </Route>
+                <Route exact path="/pay">
+                    <Payment />
+                </Route>
+            </Switch>
+        </PaymentSessionProvider>
+    );
 }
 
 export default App;
--- shopify/frontend/src/index.tsx
+++ shopify/frontend/src/index.tsx
@@ -1,20 +1,13 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </StrictMode>,
+    document.getElementById('app')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
