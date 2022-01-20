33dbf18617cc06ca2be9655faacfb4f2f47151ed
Colin Ogoo <ogoo.colin@gmail.com>
2022-01-20 11:21:40 +0000

    docs(tsdoc find transaction signature): tsdoc strings for find transaction signature

--- core/src/findTransactionSignature.ts
+++ core/src/findTransactionSignature.ts
@@ -1,9 +1,20 @@
 import { ConfirmedSignatureInfo, Connection, Finality, PublicKey, SignaturesForAddressOptions } from '@solana/web3.js';
 
+/** @ignore */
 export class FindTransactionSignatureError extends Error {
     name = 'FindTransactionSignatureError';
 }
 
+/**
+ * Find a transaction signature
+ *
+ * @throws if signature can't be found
+ *
+ * @param connection - A connection to the cluster.
+ * @param reference - A `PublicKey` that was included as a reference in the transaction.
+ * @param {SignaturesForAddressOptions} options - Options for getSignaturesForAddress.
+ * @param finality - A subset of Commitment levels, which are at least optimistically confirmed
+ */
 export async function findTransactionSignature(
     connection: Connection,
     reference: PublicKey,
