ceaa8dd5c54be3aa20d5965fc45d5e2a00de631d
Jordan Sexton <jordan@jordansexton.com>
2022-01-20 19:01:02 -0600

    fix transactions link

--- point-of-sale/src/components/TransactionsLink.tsx
+++ point-of-sale/src/components/TransactionsLink.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Activity } from './Activity';
 import * as styles from './TransactionsLink.module.css';
 
 export const TransactionsLink: FC = () => {
+    const { config } = useParams();
     return (
-        <Link to="transactions" className={styles.link}>
+        <Link to={`/${config}/transactions`} className={styles.link}>
             <span className={styles.icon}>
                 <Activity />
             </span>
