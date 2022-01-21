ffecbd00ea2e614c4e9e9ecab5104b79ab14affb
Jordan Sexton <jordan@jordansexton.com>
2022-01-21 15:39:27 -0600

    add hook to link preserving query params

---
+++ point-of-sale/src/hooks/useLinkWithQuery.ts
@@ -0,0 +1,9 @@
+import { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export function useLinkWithQuery (pathname: string) {
+    const { search } = useLocation();
+    return useMemo(() => ({ pathname, search }), [pathname, search]);
+}
+
+
