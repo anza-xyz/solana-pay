2822d47b6efc40386c4485473896139bfbbf155b
Jordan Sexton <jordan@jordansexton.com>
2022-01-21 21:49:01 -0600

    make icons smaller

--- point-of-sale/src/components/images/BackIcon.tsx
+++ point-of-sale/src/components/images/BackIcon.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 
 export const BackIcon: FC = () => {
     return (
-        <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+        <svg fill="none" height="20" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg">
             <path
                 d="m15 18-6-6 6-6"
                 stroke="currentColor"
--- point-of-sale/src/components/images/MaximizeIcon.tsx
+++ point-of-sale/src/components/images/MaximizeIcon.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 
 export const MaximizeIcon: FC = () => {
     return (
-        <svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+        <svg fill="none" height="20" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg">
             <g stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
                 <path d="m15 3h6v6" />
                 <path d="m9 21h-6v-6" />
