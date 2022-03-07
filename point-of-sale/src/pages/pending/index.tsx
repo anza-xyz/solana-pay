06ddb5457746b50c79c300f46f5af13af5053ddb
Callum McIntyre <callum@pointer.gg>
2022-03-07 11:05:46 +0000

    Add empty getServerSideProps to /pending to make getInitialProps run on the server

--- point-of-sale/src/pages/pending/index.tsx
+++ point-of-sale/src/pages/pending/index.tsx
@@ -9,6 +9,15 @@ import { PoweredBy } from '../../components/sections/PoweredBy';
 import { QRCode } from '../../components/sections/QRCode';
 import css from './pending.module.css';
 
+export function getServerSideProps() {
+    // Required so getInitialProps re-runs on the server-side
+    // If it runs on client-side then there's no req and the URL reading doesn't work
+    // See https://nextjs.org/docs/api-reference/data-fetching/get-initial-props
+    return {
+        props: {}
+    }
+}
+
 export default function PendingRoute() {
     const { symbol, connectWallet } = useConfig();
     const { amount, reset } = usePayment();
