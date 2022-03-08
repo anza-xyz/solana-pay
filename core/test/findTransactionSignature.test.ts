31e0c7e917c80b606ad26234f87a219654e65a30
Timothy Odei Yirenkyi <t.o.yirenkyi@gmail.com>
2022-03-08 12:56:58 +0000

    update findReference test suite

--- core/test/findTransactionSignature.test.ts
+++ core/test/findTransactionSignature.test.ts
@@ -1,28 +1,49 @@
-import { Connection, Keypair, PublicKey } from '@solana/web3.js';
+import {
+    Connection,
+    PublicKey,
+    Keypair,
+    SignaturesForAddressOptions,
+    Finality,
+    ConfirmedSignatureInfo,
+    clusterApiUrl,
+} from '@solana/web3.js';
 import { findReference } from '../src';
 
 const reference = Keypair.generate().publicKey;
 const signaturesForAddress = {
-    [reference.toBase58()]: [{ signature: 'signature' }],
+    [reference.toBase58()]: [
+        { signature: 'signature1', blockTime: 0, slot: 0, memo: 'memo', err: null },
+        { signature: 'signature2', blockTime: 0, slot: 0, memo: 'memo', err: null },
+    ],
 };
 
-const connection = {
-    async getSignaturesForAddress(reference: PublicKey) {
-        return signaturesForAddress[reference.toBase58()] || [];
-    },
-} as Connection;
+jest.spyOn(Connection.prototype, 'getSignaturesForAddress').mockImplementation(
+    async (
+        address: PublicKey,
+        _options?: SignaturesForAddressOptions,
+        _commitment?: Finality
+    ): Promise<ConfirmedSignatureInfo[]> => {
+        return signaturesForAddress[address.toBase58()] || [];
+    }
+);
 
 describe('findTransactionSignature', () => {
     it('should return the last signature', async () => {
         expect.assertions(1);
 
-        const found = await findReference(connection, reference);
+        const endpoint = clusterApiUrl('devnet');
+        const connection = new Connection(endpoint);
 
-        expect(found).toEqual({ signature: 'signature' });
+        const found = await findReference(connection, reference );
+
+        expect(found).toEqual({ signature: 'signature2', blockTime: 0, slot: 0, memo: 'memo', err: null });
     });
 
     it('throws an error on signature not found', async () => {
         expect.assertions(1);
+
+        const endpoint = clusterApiUrl('devnet');
+        const connection = new Connection(endpoint);
 
         const reference = Keypair.generate().publicKey;
 
