3884c639f2f81613cfd13eb805d97b43d6f06d83
Flo <flodef@free.fr>
2022-07-30 02:39:44 +0200

    Added some French translation + currency Name

--- point-of-sale/src/client/components/sections/Summary.tsx
+++ point-of-sale/src/client/components/sections/Summary.tsx
@@ -5,16 +5,20 @@ import { Amount } from './Amount';
 import css from './Summary.module.css';
 
 export const Summary: FC = () => {
-    const { symbol } = useConfig();
+    const { symbol, curName } = useConfig();
     const { amount } = usePayment();
 
+    const showSymbol = Boolean(process.env.NEXT_PUBLIC_SHOW_SYMBOL) || false;
+    const currency = showSymbol ? symbol : curName;
+
+    //TODO : Add translastion
     return (
         <div className={css.root}>
-            <div className={css.title}>Balance Due</div>
+            <div className={css.title}>Solde</div>
             <div className={css.total}>
                 <div className={css.totalLeft}>Total</div>
                 <div className={css.totalRight}>
-                    <div className={css.symbol}>{symbol}</div>
+                    <div className={css.symbol}>{currency}</div>
                     <div className={css.amount}>
                         <Amount amount={amount} />
                     </div>
