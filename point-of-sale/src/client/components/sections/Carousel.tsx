69bc5121b417d4fc7fc352272cd8fc48e7a0ff69
Flo <flodef@free.fr>
2022-08-15 01:13:17 +0200

    Les paramètres URL sont ajoutés par la méthode standard (encodage des caractères spéciaux)

--- point-of-sale/src/client/components/sections/Carousel.tsx
+++ point-of-sale/src/client/components/sections/Carousel.tsx
@@ -4,17 +4,24 @@ import { Carousel } from 'react-responsive-carousel';
 import { Merchant, MerchantInfo, MerchantProps } from './Merchant';
 import css from './Carousel.module.css';
 import { useNavigateWithQuery } from '../../hooks/useNavigateWithQuery';
+import { useConfig } from '../../hooks/useConfig';
 
 export interface MerchantsProps {
     merchants: MerchantInfo[];
 }
 
 export const MerchantCarousel: FC<MerchantsProps> = ({ merchants }) => {
+    const { baseURL } = useConfig();
     const navigate = useNavigateWithQuery();
     const merchantList = useMemo(() => merchants, []);
     const onClickItem = (index: number, item: ReactNode) => {
         const { index: id, address: recipient, company: label, maxValue } = merchantList[index];
-        navigate('/new?id=' + id + '&recipient=' + recipient + '&label=' + label + '&maxValue=' + maxValue);
+        const url = new URL(`${baseURL}/new`);
+        url.searchParams.append('id', id.toString());
+        url.searchParams.append('recipient', recipient.toString());
+        url.searchParams.append('label', label.toString());
+        url.searchParams.append('maxValue', maxValue.toString());
+        navigate(url.toString());
     };
 
     return (
