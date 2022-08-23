15f76df0f63048d4432f4c27bf1ec5db9f92d52b
Jordan Sexton <jordan@jordansexton.com>
2022-08-23 12:40:46 -0500

    validate memo instruction before transfer instruction

--- core/src/validateTransfer.ts
+++ core/src/validateTransfer.ts
@@ -12,6 +12,7 @@ import {
     Message,
     SystemInstruction,
     Transaction,
+    TransactionInstruction,
     TransactionResponse,
     TransactionSignature,
 } from '@solana/web3.js';
@@ -70,33 +71,49 @@ export async function validateTransfer(
         reference = [reference];
     }
 
-    const [preAmount, postAmount] = splToken
-        ? await validateSPLTokenTransfer(message, meta, recipient, splToken, reference)
-        : await validateSystemTransfer(message, meta, recipient, reference);
-    if (postAmount.minus(preAmount).lt(amount)) throw new ValidateTransferError('amount not transferred');
+    const transaction = Transaction.populate(message);
+    // TODO: could _potentially_ support instructions at arbitrary indexes
+    let instructionIndex = 0;
 
-    if (memo) {
-        // Check that the second instruction is a memo instruction with the expected memo.
-        const transaction = Transaction.populate(message);
-        const instruction = transaction.instructions[1];
-        if (!instruction) throw new ValidateTransferError('missing memo instruction');
-        if (!instruction.programId.equals(MEMO_PROGRAM_ID)) throw new ValidateTransferError('invalid memo program');
-        if (!instruction.data.equals(Buffer.from(memo, 'utf8'))) throw new ValidateTransferError('invalid memo');
+    if (memo !== undefined) {
+        validateMemo(transaction.instructions[instructionIndex], memo);
+        instructionIndex++;
     }
+
+    const [preAmount, postAmount] = splToken
+        ? await validateSPLTokenTransfer(
+              transaction.instructions[instructionIndex],
+              message,
+              meta,
+              recipient,
+              splToken,
+              reference
+          )
+        : await validateSystemTransfer(transaction.instructions[instructionIndex], message, meta, recipient, reference);
+    if (postAmount.minus(preAmount).lt(amount)) throw new ValidateTransferError('amount not transferred');
 
     return response;
 }
 
+function validateMemo(instruction: TransactionInstruction, memo: string): void {
+    // Check that the instruction is a memo instruction with the expected memo.
+    if (!instruction) throw new ValidateTransferError('missing memo instruction');
+    if (!instruction.programId.equals(MEMO_PROGRAM_ID)) throw new ValidateTransferError('invalid memo program');
+    if (!instruction.data.equals(Buffer.from(memo, 'utf8'))) throw new ValidateTransferError('invalid memo');
+}
+
 async function validateSystemTransfer(
+    instruction: TransactionInstruction,
     message: Message,
     meta: ConfirmedTransactionMeta,
     recipient: Recipient,
     references?: Reference[]
 ): Promise<[BigNumber, BigNumber]> {
+    const accountIndex = message.accountKeys.findIndex((pubkey) => pubkey.equals(recipient));
+    if (accountIndex === -1) throw new ValidateTransferError('recipient not found');
+
     if (references) {
-        // Check that the first instruction is a system transfer instruction.
-        const transaction = Transaction.populate(message);
-        const instruction = transaction.instructions[0];
+        // Check that the instruction is a system transfer instruction.
         SystemInstruction.decodeTransfer(instruction);
 
         // Check that the expected reference keys exactly match the extra keys provided to the instruction.
@@ -109,9 +126,6 @@ async function validateSystemTransfer(
         }
     }
 
-    const accountIndex = message.accountKeys.findIndex((pubkey) => pubkey.equals(recipient));
-    if (accountIndex === -1) throw new ValidateTransferError('recipient not found');
-
     return [
         new BigNumber(meta.preBalances[accountIndex] || 0).div(LAMPORTS_PER_SOL),
         new BigNumber(meta.postBalances[accountIndex] || 0).div(LAMPORTS_PER_SOL),
@@ -119,21 +133,25 @@ async function validateSystemTransfer(
 }
 
 async function validateSPLTokenTransfer(
+    instruction: TransactionInstruction,
     message: Message,
     meta: ConfirmedTransactionMeta,
     recipient: Recipient,
     splToken: SPLToken,
     references?: Reference[]
 ): Promise<[BigNumber, BigNumber]> {
+    const recipientATA = await getAssociatedTokenAddress(splToken, recipient);
+    const accountIndex = message.accountKeys.findIndex((pubkey) => pubkey.equals(recipientATA));
+    if (accountIndex === -1) throw new ValidateTransferError('recipient not found');
+
     if (references) {
         // Check that the first instruction is an SPL token transfer instruction.
-        const transaction = Transaction.populate(message);
-        const instruction = decodeInstruction(transaction.instructions[0]);
-        if (!isTransferCheckedInstruction(instruction) && !isTransferInstruction(instruction))
+        const decodedInstruction = decodeInstruction(instruction);
+        if (!isTransferCheckedInstruction(decodedInstruction) && !isTransferInstruction(decodedInstruction))
             throw new ValidateTransferError('invalid transfer');
 
         // Check that the expected reference keys exactly match the extra keys provided to the instruction.
-        const extraKeys = instruction.keys.multiSigners;
+        const extraKeys = decodedInstruction.keys.multiSigners;
         const length = extraKeys.length;
         if (length !== references.length) throw new ValidateTransferError('invalid references');
 
@@ -141,10 +159,6 @@ async function validateSPLTokenTransfer(
             if (!extraKeys[i].pubkey.equals(references[i])) throw new ValidateTransferError(`invalid reference ${i}`);
         }
     }
-
-    const recipientATA = await getAssociatedTokenAddress(splToken, recipient);
-    const accountIndex = message.accountKeys.findIndex((pubkey) => pubkey.equals(recipientATA));
-    if (accountIndex === -1) throw new ValidateTransferError('recipient not found');
 
     const preBalance = meta.preTokenBalances?.find((x) => x.accountIndex === accountIndex);
     const postBalance = meta.postTokenBalances?.find((x) => x.accountIndex === accountIndex);
