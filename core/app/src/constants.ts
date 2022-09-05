a0ada31571447679a0491b8cf06e26c39e85c01c
Niten619 <nitenlama46@gmail.com>
2022-09-05 14:17:34 +0545

    solana pay react app

---
+++ core/app/src/constants.ts
@@ -0,0 +1,17 @@
+import { PublicKey } from '@solana/web3.js';
+import BigNumber from 'bignumber.js';
+
+/** @internal */
+export const SOLANA_PROTOCOL = 'solana:';
+
+/** @internal */
+export const HTTPS_PROTOCOL = 'https:';
+
+/** @internal */
+export const MEMO_PROGRAM_ID = new PublicKey('MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr');
+
+/** @internal */
+export const SOL_DECIMALS = 9;
+
+/** @internal */
+export const TEN = new BigNumber(10);
