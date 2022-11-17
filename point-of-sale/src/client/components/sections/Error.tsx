492e070abe99756007bde5a61fbaf3037be0e638
Flo <flodef@free.fr>
2022-11-17 03:26:07 +0100

    PAY : Ajout de textes pour nouvelles erreurs

--- point-of-sale/src/client/components/sections/Error.tsx
+++ point-of-sale/src/client/components/sections/Error.tsx
@@ -15,12 +15,18 @@ export const Error: FC = () => {
                     return 'Vous avez refusé la transaction !';
                 case 'WalletSendTransactionError':
                     return 'Vous avez trop tardé à approuver la transaction !';
+                case 'TokenAccountNotFoundError':
+                    return 'Le marchand doit ajouter cette monnaie à son porte-monnaie !';
                 case 'CreateTransferError':
                     return e[1] === 'insufficient funds'
                         ? 'Le montant est supérieur à vos fonds !'
                         : e[1] === 'recipient not found'
-                            ? "Le porte-monnaie de ce commerçant à besoin d'être initialisé !"
+                            ? "Le porte-monnaie de ce commerçant a besoin d'être initialisé !"
                             : 'Erreur de transfert !';
+                case 'Error':
+                    return e[1].trim() === '429'
+                        ? 'Le réseau est momentanément saturé, merci de réessayer !'
+                        : 'Erreur inconnue : ' + errorMessage;
                 default:
                     return 'Erreur inconnue : ' + errorMessage;
             }
