b8d2ed4a983a47bbdb3fee8ddf41adb14b143ebe
Flo <flodef@free.fr>
2022-11-30 02:32:41 +0100

    PAY : Bouton "Payer" disparaît si porte-monnaie déconnecté

--- point-of-sale/src/client/components/buttons/GenerateButton.tsx
+++ point-of-sale/src/client/components/buttons/GenerateButton.tsx
@@ -1,3 +1,4 @@
+import { useWallet } from "@solana/wallet-adapter-react";
 import React, { FC, MouseEventHandler, ReactNode, useCallback, useMemo } from 'react';
 import { PaymentStatus, usePayment } from '../../hooks/usePayment';
 import css from './GenerateButton.module.css';
@@ -8,13 +9,14 @@ export interface GenerateButtonProps {
 
 export const GenerateButton: FC<GenerateButtonProps> = ({ children }) => {
     const { amount, status, generate } = usePayment();
+    const { publicKey } = useWallet();
 
     return (
         <button
             className={css.root}
             type="button"
             onClick={generate}
-            disabled={!amount || amount.isLessThanOrEqualTo(0) || (status !== PaymentStatus.New && status !== PaymentStatus.Error)}
+            disabled={!publicKey || !amount || amount.isLessThanOrEqualTo(0) || (status !== PaymentStatus.New && status !== PaymentStatus.Error)}
         >
             {children}
         </button>
