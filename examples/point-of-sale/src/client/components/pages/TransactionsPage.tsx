42cdd907347d6cd025462da6787d95c5e6ee7c97
Flo <flodef@free.fr>
2022-12-30 15:30:10 +0100

    PAY: Recent transaction disappear when clicking "back"

--- examples/point-of-sale/src/client/components/pages/TransactionsPage.tsx
+++ examples/point-of-sale/src/client/components/pages/TransactionsPage.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { MouseEventHandler, useCallback, useState } from 'react';
 import { FormattedMessage } from "react-intl";
 import { BackButton } from '../buttons/BackButton';
 import { PoweredBy } from '../sections/PoweredBy';
@@ -9,13 +9,22 @@ import css from './TransactionsPage.module.css';
 
 const TransactionsPage: NextPage = () => {
     const router = useRouter();
+    const [disabled, setDisabled] = useState(false);
+    const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+        (event) => {
+            setDisabled(true);
+            router.back();
+        },
+        [router]
+    );
+
 
     return (
         <div className={css.root}>
             <div className={css.header}>
-                <BackButton onClick={router.back}><FormattedMessage id="back" /></BackButton>
+                <BackButton onClick={handleClick}><FormattedMessage id="back" /></BackButton>
             </div>
-            <div className={css.main}>
+            <div className={!disabled ? css.main : css.mainHidden}>
                 <Transactions />
             </div>
             <PoweredBy />
