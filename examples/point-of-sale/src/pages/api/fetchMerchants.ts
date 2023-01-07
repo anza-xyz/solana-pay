d80c0ad3175c73bc36d11654ef29fcc5a350ad30
Flo <flodef@free.fr>
2023-01-07 20:56:35 +0100

    PAY: Merchant.json has a spreadsheet format

--- examples/point-of-sale/src/pages/api/fetchMerchants.ts
+++ examples/point-of-sale/src/pages/api/fetchMerchants.ts
@@ -7,12 +7,24 @@ interface GetResponse {
     company: string;
     currency: string;
     maxValue: number;
+    location: string;
 }
 
 const fetchMerchants: NextApiHandler<GetResponse[] | undefined> = async (request, response) => {
     // If get request
     if (request.method === 'GET') {
-        response.status(200).json(merchants.map((merchant) => merchant));
+        if (merchants.values.length <= 1) throw new Error('incomplete data');
+        const labels = merchants.values[0];
+
+        response.status(200).json(merchants.values.filter((merchant, i) => i !== 0).map((merchant, i) => {
+            const index = Number(merchant[labels.indexOf("index")]);
+            const address = String(merchant[labels.indexOf("address")]);
+            const company = String(merchant[labels.indexOf("company")]);
+            const currency = String(merchant[labels.indexOf("currency")]);
+            const maxValue = Number(merchant[labels.indexOf("maxValue")]);
+            const location = String(merchant[labels.indexOf("location")]);
+            return { index, address, company, currency, maxValue, location };
+        }));
     } else {
         throw new Error(`Method ${request.method} not allowed`);
     }
--- examples/point-of-sale/src/pages/api/findMerchant.ts
+++ examples/point-of-sale/src/pages/api/findMerchant.ts
@@ -6,6 +6,7 @@ interface GetResponse {
     company: string;
     currency: string;
     maxValue: number;
+    location: string;
 }
 
 const findMerchant: NextApiHandler<GetResponse | undefined> = async (request, response) => {
@@ -14,11 +15,17 @@ const findMerchant: NextApiHandler<GetResponse | undefined> = async (request, re
         const id = Number(request.query.id);
         if (!id) throw new Error('missing id');
         if (typeof id !== 'number') throw new Error('invalid id');
+        if (merchants.values.length <= 1) throw new Error('incomplete data');
 
-        const merchant = merchants.find((merchant) => merchant.index === id);
+        const labels = merchants.values[0];
+        const merchant = merchants.values.find(merchant => merchant[labels.indexOf("index")] === id);
         if (merchant) {
-            const { address, company, currency, maxValue } = merchant;
-            response.status(200).json({ address, company, currency, maxValue });
+            const address = String(merchant[labels.indexOf("address")]);
+            const company = String(merchant[labels.indexOf("company")]);
+            const currency = String(merchant[labels.indexOf("currency")]);
+            const maxValue = Number(merchant[labels.indexOf("maxValue")]);
+            const location = String(merchant[labels.indexOf("location")]);
+            response.status(200).json({ address, company, currency, maxValue, location });
         } else {
             // 204 = successfully processed the request, not returning any content
             response.status(204).send(undefined);
