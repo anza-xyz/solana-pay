57aa171f7e3a986f537f52393980a5300c32f089
Callum McIntyre <callum.mcintyre@solana.com>
2023-07-17 10:55:32 +0100

    Replace Discord link with Stack Exchange

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -81,8 +81,8 @@ const config = {
                         title: 'Community',
                         items: [
                             {
-                                label: 'Discord',
-                                href: 'https://solana.com/discord',
+                                label: 'Stack Exchange',
+                                href: 'https://solana.stackexchange.com',
                             },
                             {
                                 label: 'Twitter',
